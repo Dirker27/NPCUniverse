e74c919bf8ba9395e340b30503faa176643f59cb
Dirk <hortensd@gmail.com>
2016-02-20 16:56:20 -0800

    Basic Player Movement Scripts

---
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+    public NavigationWaypoint destination;
+    public float travelRate = 1; // [m/s]
+    public bool twoDimensionalMovement = true;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (destination != null)
+        {
+            Move();
+        }
+
+        if (Vector3.Distance(destination.transform.position, transform.position) < 3f)
+        {
+            destination = null;
+        }
+	}
+
+    private void Move()
+    {
+        Vector3 dPos = destination.transform.position;
+        Vector3 myPos = transform.position;
+        if (twoDimensionalMovement)
+        {
+            myPos.y = 0;
+            dPos.y = 0;
+        }
+        Vector3 directionVector = (dPos - myPos) / Vector3.Distance(dPos, myPos);
+
+        float rate = Time.deltaTime * travelRate;
+
+        transform.localPosition += directionVector * rate;
+    }
+
+    public bool isInTransit()
+    {
+        return destination != null;
+    }
+}
