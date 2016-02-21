d524b23eb82149a964c3ee6de817d63afbaa7895
Dirk <hortensd@gmail.com>
2016-02-20 19:29:54 -0800

    Fuckington Palace
    Where you wanna come

---
+++ Assets/Scripts/Character/Inventory.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour
+{
+    public int currency;
+    //public List<Item> Items { get; set; }
+}
---
+++ Assets/Scripts/Character/NonPlayableCharacter.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NonPlayableCharacter : MonoBehaviour
+{
+    public Inventory Inventory { get; set; }
+    
+}
