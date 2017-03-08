46d5b4603ae7460b97bb99116c542d39433fc7c5
Dirk <hortensd@gmail.com>
2017-03-07 23:36:03 -0800

    Units moving using Navigation Mesh

---
+++ Assets/Character/Unit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class Unit : MonoBehaviour {
+
+    public float health = 100f;
+    public float movementSpeed = 1f; // [m/s]
+    public float turnSpeed = 1f;
+    public float jumpHeight = 1f; // [m]
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Character/WaypointMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Unit))]
+[RequireComponent(typeof(NavMeshAgent))]
+public class WaypointMovement : MonoBehaviour {
+
+    public float arrivalRadius = 5f;
+    public GameObject currentWaypoint;
+
+    private NavMeshAgent agent;
+    private Unit unit;
+
+	// Use this for initialization
+	void Start ()
+    {
+        unit = GetComponent<Unit>();
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (currentWaypoint != null)
+        {
+            moveTowardsWaypoint();
+        }
+    }
+
+
+    private void moveTowardsWaypoint()
+    {
+        //TODO: remove
+        agent.destination = currentWaypoint.transform.position;
+
+        Vector3 worldDelta = agent.nextPosition - transform.position;
+        if (worldDelta.magnitude < arrivalRadius)
+        {
+            currentWaypoint = null;
+            return;
+        }
+
+        /*
+        float turnSpeed = Vector3.Dot(Vector3.up, Vector3.Cross(transform.forward, worldDelta));
+        float forwardSpeed = Vector3.Dot(transform.forward, worldDelta);
+        */
+
+        Vector3 targetDirection = worldDelta / worldDelta.magnitude;
+        transform.position += targetDirection * unit.movementSpeed * Time.deltaTime;
+    }
+
+    /*public void AssignWaypoint(GameObject waypoint)
+    {
+        currentWaypoint = waypoint;
+        agent.SetDestination(waypoint.transform.position);
+    }*/
+}
